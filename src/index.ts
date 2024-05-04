9c51c703baaa403db5d4f795428d10a6497fd361
htmlucas <lucasmartinsde@gmail.com>
2024-05-04 06:33:20 -0300

    voltando os requires para import

--- src/index.ts
+++ src/index.ts
@@ -1,15 +1,21 @@
-require('reflect-metadata');
-const express = require('express');
-const { router } = require('./routes');
-const cors = require('cors');
+import 'reflect-metadata'
+import express, { Request, Response} from 'express'
+import { router } from './routes';
+import cors from 'cors';
 
 const server = express();
 
-server.use(cors());
+server.use(cors())
 
-server.use(express.json());
-server.use(router);
+server.use(express.json())
+server.use(router)
+
+server.get('/',(request: Request, response: Response) => {
+    return response.status(200).json({ message: 'Acessou e retornou tudo certo'})
+})
+
 
 server.listen(5000, () => {
-    console.log('Online');
-});
+    console.log('Online')
+})
+
