2b5e0d3c18be3bb68c4760f47841cbc430577bc0
htmlucas <lucasmartinsde@gmail.com>
2024-05-04 06:30:05 -0300

    voltando o package e utilizando require

--- src/index.ts
+++ src/index.ts
@@ -1,21 +1,15 @@
-import 'reflect-metadata';
-import express, { Request, Response} from 'express'
-import { router } from './routes';
-import cors from 'cors';
+require('reflect-metadata');
+const express = require('express');
+const { router } = require('./routes');
+const cors = require('cors');
 
 const server = express();
 
-server.use(cors())
+server.use(cors());
 
-server.use(express.json())
-server.use(router)
-
-server.get('/',(request: Request, response: Response) => {
-    return response.status(200).json({ message: 'Acessou e retornou tudo certo'})
-})
-
+server.use(express.json());
+server.use(router);
 
 server.listen(5000, () => {
-    console.log('Online')
-})
-
+    console.log('Online');
+});
