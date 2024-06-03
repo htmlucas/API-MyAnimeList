88fc02e6bf5a55833cfeaf891aa95e0396a6b448
htmlucas <lucasmartinsde@gmail.com>
2024-06-03 19:14:57 -0300

    fix: corrige bug na verificação de usuario logado

--- src/middleware/verifyAuth.ts
+++ src/middleware/verifyAuth.ts
@@ -1,15 +1,13 @@
 import { NextFunction, Request, Response } from "express";
 import { verify } from "jsonwebtoken";
 
-export function verifyAuth (request: Request, response: Response, next: NextFunction) {
+export function verifyAuth (request: Request, response: Response, next: NextFunction) {    
     const authToken = request.headers.authorization
 
     if(authToken){
-        const [, token] = authToken.split(' ')
 
         try {
-            const { sub } = verify(token,'123456789')
-            console.log('Token for user', sub)
+            verify(authToken,'123456789')
             return next()
         } catch (error) {
             return response.status(401).json({message: 'unauthorized'})
